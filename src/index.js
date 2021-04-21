8dd33c80e799bfd4fb83e07f4e7fa91393e5264b
NANIRISU <39872073+NANIRISU@users.noreply.github.com>
2021-04-21 23:18:59 +0530

    added search bar added array to store api  data and operation for search user firstname

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const api = `https://randomuser.me/api`;
 const adduser = document.getElementById("add-btn");
-const mainApp = document.getElementById("app");
+//const mainApp = document.getElementById("app");
+const userlist = document.getElementById("user-list");
+const searchInput = document.getElementById("search");
 console.log(adduser, "add");
+
+const appState = [];
+
 adduser.addEventListener("click", async () => {
   const userData = await fetch(api, {
     method: "GET"
@@ -10,10 +15,31 @@ adduser.addEventListener("click", async () => {
   });
   const userJson = await userData.json();
   console.log(userJson.results[0], "json");
-  const userElement = document.createElement("div");
   const user = userJson.results[0];
-  userElement.innerHTML = `<div>
-  ${user.name.title} ${user.name.first}
+  appState.push(user);
+  console.log(appState, "appState");
+  domRender(appState);
+});
+
+const domRender = (stateArr) => {
+  userlist.innerHTML = null;
+  stateArr.forEach((userObj) => {
+    const userElement = document.createElement("div");
+
+    userElement.innerHTML = `<div>
+  ${userObj.name.title} ${userObj.name.first}
    </div>`; // newline
-  mainApp.appendChild(userElement);
+    userlist.appendChild(userElement);
+  });
+};
+
+// const serchFilter=()=>{
+
+// }
+searchInput.addEventListener("keyup", (e) => {
+  console.log(e, searchInput.value);
+  const filteredAppState = appState.filter((user) =>
+    user.name.first.toLowerCase().includes(searchInput.value.toLowerCase())
+  );
+  domRender(filteredAppState);
 });
