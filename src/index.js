dc834293beef75c6fd032ac8fc1af75d3b4bb83b
NANIRISU <39872073+NANIRISU@users.noreply.github.com>
2021-04-22 23:07:04 +0530

    added firstname and lastname and email gender then filter based name

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const api = `https://randomuser.me/api`;
 const adduser = document.getElementById("add-btn");
-//const mainApp = document.getElementById("app");
+const sortDes = document.getElementById("sort-des");
+const sortAes = document.getElementById("sort-aes");
 const userlist = document.getElementById("user-list");
 const searchInput = document.getElementById("search");
 console.log(adduser, "add");
 
 const appState = [];
+
+class Usercls {
+  constructor(title, firstname, lastname, gender, email) {
+    this.name = ` ${firstname} ${lastname}`;
+    this.title = `${title}`;
+    this.email = email;
+    this.gender = gender;
+  }
+}
 
 adduser.addEventListener("click", async () => {
   const userData = await fetch(api, {
@@ -16,7 +26,16 @@ adduser.addEventListener("click", async () => {
   const userJson = await userData.json();
   console.log(userJson.results[0], "json");
   const user = userJson.results[0];
-  appState.push(user);
+
+  const classUser = new Usercls(
+    user.name.title,
+    user.name.first,
+    user.name.last,
+    user.gender,
+    user.email
+  );
+  //appState.push(user);
+  appState.push(classUser);
   console.log(appState, "appState");
   domRender(appState);
 });
@@ -27,8 +46,9 @@ const domRender = (stateArr) => {
     const userElement = document.createElement("div");
 
     userElement.innerHTML = `<div>
-  ${userObj.name.title} ${userObj.name.first}
-   </div>`; // newline
+  ${userObj.name}
+ <ol><li>${userObj.gender}</li>
+ <li>  ${userObj.email} </li> </ol   </div>`; // newline
     userlist.appendChild(userElement);
   });
 };
@@ -38,8 +58,29 @@ const domRender = (stateArr) => {
 // }
 searchInput.addEventListener("keyup", (e) => {
   console.log(e, searchInput.value);
-  const filteredAppState = appState.filter((user) =>
-    user.name.first.toLowerCase().includes(searchInput.value.toLowerCase())
+
+  const filteredAppState = appState.filter(
+    (user) =>
+      // user.name.first.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      //user.name.last.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.name.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.gender.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchInput.value.toLowerCase())
+    // &
+    // user.sort(user.name.first)
   );
+
   domRender(filteredAppState);
+});
+
+sortDes.addEventListener("click", () => {
+  var state = [...appState];
+  state.sort((a, b) => (a.name.first < b.name.last ? -1 : 1));
+  domRender(state);
+});
+//sortAes;
+sortAes.addEventListener("click", () => {
+  var state = [...appState];
+  state.sort((a, b) => (a.name.first < b.name.last ? 1 : -1));
+  domRender(state);
 });
